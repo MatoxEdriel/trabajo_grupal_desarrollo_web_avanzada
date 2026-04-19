b7169e3a092bbee9237c8d656c1c18c0ceb0745b
MatoxEdriel <ga99_buggi@hotmail.com>
2026-04-18 20:21:26 -0500

    yutita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/Auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {
+        path: 'login', redirectTo: 'login', pathMatch: 'full'
+
+    },
+    {
+
+        path: 'login', component: LoginComponent
+    }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./modules/Auth/login/login.component";
+import { NavComponent } from "./shared/components/nav/nav.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
